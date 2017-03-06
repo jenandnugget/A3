633ea77c6e2d872bbddb2160bb43b9b6d080e2b1
nuggetsnegin <nuggetnegin@gmail.com>
2017-03-06 18:48:16 -0400

    Apriori
    Added scanner

--- src/Apriori.java
+++ src/Apriori.java
@@ -1,41 +1,40 @@
 //base project, Negin Sauermann section
 //Referenced Apriori Algorithm by Nathan Magnus, 2009 (http://www2.cs.uregina.ca/~dbd/cs831/notes/itemsets/itemset_prog1.html)
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Apriori {
 	//necessary declarations
-	double minSupp; //minimum support for frequent item sets
-	double minRate; //minimum confidence rate for declaration 
+	double minSupp = 0.0; //minimum support for frequent item sets
+	double minRate = 0.0; //minimum confidence rate for declaration 
 	
 	String inputFile = ""; 
 	String outputFile = ""; 
 	
-	double currentItemS;
-	
-	ArrayList<dataNode> rulesList;
+	double currentItemS = 0;
 	ArrayList<String> data;
 	ArrayList<String> dataType;
+	
+	//User input
+	Scanner keyboard = new Scanner(System.in);
 
-	//initializing 
-	public Apriori(){
-		minSupp = 0.0;
-		minRate = 0.0;
-		currentItemS = 0;
-		String inputFile = "file.txt";
-		String outputFile = "output.txt"; 
+	System.out.println("What is the file name for your data?");
+	inputFile = keyboard.nextLine();
 
-		rulesList = new ArrayList<dataNode>();
+	System.out.println("Select the minium support rate(Only values from 0.00-1.00):");
+	minRate = keyboard.nextDouble();
 
-		startData = new ArrayList<String>();
-		dataType = new ArrayList<String>();
-		data = new ArrayList<String>();
-	}
+	System.out.println("Please select the minimum confidence rate(Only values from 0.00-1.00):");
+	minSupp = keyboard.nextDouble();
+
+	keyboard.close();
+	
+	
 
 	
 }
